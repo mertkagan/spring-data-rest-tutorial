6f563f7c8d75fcebb28af7213dd3f2427e9a42c6
Mert Kağan Aktaş <mertkagan77@gmail.com>
2023-11-06 11:20:26 +0300

    Added Swagger
    "Added Swagger documentation"

--- springDataRest/src/main/java/com/mertkagan/springDataRest/SpringDataRestApplication.java
+++ springDataRest/src/main/java/com/mertkagan/springDataRest/SpringDataRestApplication.java
@@ -2,8 +2,10 @@ package com.mertkagan.springDataRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringDataRestApplication {
 
 	public static void main(String[] args) {
